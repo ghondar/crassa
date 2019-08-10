453da152d6184069882546b5b021922d54fd8850
Anthony Bryan Gavilan Vinces <abgv9221@gmail.com>
2019-08-09 20:55:18 -0500

    [ FIX ] config-override mode production

--- config-overrides.js
+++ config-overrides.js
@@ -14,34 +14,35 @@ Object.keys(_moduleAliases).forEach(key => {
   aliases[key] = _moduleAliases[key].replace('.', appRootPath)
 })
 
+const addLoadablePlugin =  config => {
+  config.plugins.push(
+    new LoadablePlugin()
+  )
+  config.plugins.shift()
+  config.plugins.unshift(
+    new HtmlWebpackPlugin({
+      inject  : false,
+      template: appPublic + '/index.html',
+      minify  : {
+        removeComments               : true,
+        collapseWhitespace           : true,
+        removeRedundantAttributes    : true,
+        useShortDoctype              : true,
+        removeEmptyAttributes        : true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash             : true,
+        minifyJS                     : true,
+        minifyCSS                    : true,
+        minifyURLs                   : true
+      }
+    })
+  )
+
+  return config
+}
+
 module.exports = override(
-  config => {
-    config.plugins.push(
-      new LoadablePlugin()
-    )
-    config.plugins.shift()
-
-    config.plugins.unshift(
-      new HtmlWebpackPlugin({
-        inject  : false,
-        template: appPublic + '/index.html',
-        minify  : {
-          removeComments               : true,
-          collapseWhitespace           : true,
-          removeRedundantAttributes    : true,
-          useShortDoctype              : true,
-          removeEmptyAttributes        : true,
-          removeStyleLinkTypeAttributes: true,
-          keepClosingSlash             : true,
-          minifyJS                     : true,
-          minifyCSS                    : true,
-          minifyURLs                   : true
-        }
-      })
-    )
-
-    return config
-  },
+  process.env.NODE_ENV === 'production' && addLoadablePlugin,
   disableEsLint(),
   ...addBabelPlugins('transform-imports', '@loadable/babel-plugin', 'transform-react-remove-prop-types'),
   addWebpackAlias({ ...aliases, 'lodash-es': 'lodash' }),
