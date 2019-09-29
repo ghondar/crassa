cdb2db77721d123838958b9e7deb123b1f39a444
Anthony Bryan Gavilan Vinces <abgv9221@gmail.com>
2019-09-28 23:21:57 -0500

    [ ADD ] flow type to server and update packages

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ require('ignore-styles')
 require('@babel/register')({
   ignore : [ /\/(build|node_modules)\/(?!@crassa\b)/ ],
   presets: [ '@babel/env', '@babel/preset-react' ],
-  plugins: [ [ '@babel/plugin-proposal-class-properties', { loose: true } ], 'babel-plugin-dynamic-import-node', '@loadable/babel-plugin' ]
+  plugins: [ [ '@babel/plugin-proposal-class-properties', { loose: true } ], '@babel/plugin-transform-flow-strip-types', 'babel-plugin-dynamic-import-node', '@loadable/babel-plugin' ]
 })
 
 require('regenerator-runtime/runtime')
