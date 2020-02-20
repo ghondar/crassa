91a516104b81c9fed8f92e0c154f69b05040c23d
Anthony Bryan Gavilan Vinces <abgv9221@gmail.com>
2020-02-20 15:39:56 -0500

    [ ADVANCE ]

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,27 +1,46 @@
 // Any route that comes in, send it to the universalLoader
 
-import express from 'express'
-import { createStore, universalLoader } from '../universal'
+// import express from 'express'
+// import { createStore, universalLoader } from '../universal'
+// import { existsSync } from 'fs'
+// import { appServer } from '../../src/paths'
+
+// const router = express.Router()
+
+// const preLoadState = appServer + '/preLoadState.js'
+// const hasPreLoadState = existsSync(preLoadState)
+// const middleware =
+//   function(req, res, next) {
+//     if(hasPreLoadState)
+//       if(req.baseUrl.indexOf('.') !== -1 || req.baseUrl.indexOf('api') !== -1 || req.baseUrl.indexOf('static') !== -1) {
+//         next()
+//       } else {
+//         require(preLoadState).default(req, res, next)
+//       }
+//     else
+//       next()
+//   }
+
+// Set universal render middlewares
+// router.use(createStore, middleware, universalLoader)
+
+// export default router
+
 import { existsSync } from 'fs'
 import { appServer } from '../../src/paths'
 
-const router = express.Router()
-
 const preLoadState = appServer + '/preLoadState.js'
 const hasPreLoadState = existsSync(preLoadState)
-const middleware =
-  function(req, res, next) {
+
+module.exports = function(f, opts, next) {
+  f.get('*', (req, res) => {
     if(hasPreLoadState)
-      if(req.baseUrl.indexOf('.') !== -1 || req.baseUrl.indexOf('api') !== -1 || req.baseUrl.indexOf('static') !== -1) {
+      if(req.originalUrl.indexOf('.') !== -1 || req.originalUrl.indexOf('api') !== -1 || req.originalUrl.indexOf('static') !== -1) {
         next()
       } else {
         require(preLoadState).default(req, res, next)
       }
-    else
-      next()
-  }
+  })
 
-// Set universal render middlewares
-router.use(createStore, middleware, universalLoader)
-
-export default router
+  next()
+}
--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
-import bodyParser from 'body-parser'
-import compression from 'compression'
-import cookieParser from 'cookie-parser'
 import { existsSync } from 'fs'
-import express from 'express'
-import morgan from 'morgan'
+import Fastify from 'fastify'
+import AutoLoad from 'fastify-autoload'
 import path from 'path'
 
-import index from './routes/index'
+// import index from './routes/index'
 
-import { appServer, appBuild } from '../src/paths'
+import { appServer/* , appBuild  */ } from '../src/paths'
 
 // Create our express app (using the port optionally specified)
-const app = express()
+const fastify = Fastify({
+  logger: true
+})
 const PORT = process.env.REACT_APP_PORT_SERVER || process.env.PORT || 5000
 const HOST = process.env.REACT_APP_HOST_SERVER || '0.0.0.0'
 
@@ -19,42 +18,61 @@ const configExpress = appServer + '/configExpress.js'
 const hasConfigExpress = existsSync(configExpress)
 
 // Compress, parse, and log
-app.use(compression())
-app.use(cookieParser())
-app.use(bodyParser.json(process.env.BODY_PARSER_LIMIT ? { limit: process.env.BODY_PARSER_LIMIT } : {}))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(morgan('dev'))
-app.disable('x-powered-by')
-const http = hasConfigExpress ? require(configExpress).default(app) : app
+fastify.register(require('fastify-cookie'))
+fastify.register(require('fastify-compress'))
+fastify.register(require('fastify-formbody'))
+// fastify.register(require('fastify-hide-powered-by'))
+const http = hasConfigExpress ? require(configExpress).default(fastify) : fastify
 
-app.use('^/$', index)
-app.use('/api', require(path.resolve(appServer)).default)
+// fastify.use('^/$', index)
+fastify.register(AutoLoad, {
+  dir: path.join(__dirname, 'routes')
+})
+
+// fastify.use('/api', require(path.resolve(appServer)).default)
+// fastify.register(AutoLoad, {
+//   dir    : path.resolve(appServer),
+//   options: {
+//     prefix: '/api/'
+//   }
+// })
 // Set up route handling, include static assets and an optional API
-app.use(express.static(path.resolve(appBuild)))
-// any other route should be handled by react-router, so serve the index page
-app.use('*', index)
+// fastify.use(express.static(path.resolve(appBuild)))
+// fastify.register(require('fastify-static'), {
+//   root  : path.resolve(appBuild),
+//   prefix: '/static/'
+// })
+// // any other route should be handled by react-router, so serve the index page
+// // fastify.use('*', index)
+// fastify.register(AutoLoad, {
+//   dir    : path.join(__dirname, 'server', 'routes'),
+//   options: {
+//     prefix: '*'
+//   }
+// })
 
 // Let's rock
-http.listen(PORT, HOST, () => {
-  console.log(`App listening on port ${PORT}!`)
+http.listen(PORT, HOST, (err, address) => {
+  if(err) throw err
+  console.log(`App listening on port ${address} ${PORT}!`)
 })
 
-// Handle the bugs somehow
-http.on('error', error => {
-  if(error.syscall !== 'listen') throw error
+// // Handle the bugs somehow
+// http.on('error', error => {
+//   if(error.syscall !== 'listen') throw error
 
-  const bind = typeof PORT === 'string' ? 'Pipe ' + PORT : 'Port ' + PORT
+//   const bind = typeof PORT === 'string' ? 'Pipe ' + PORT : 'Port ' + PORT
 
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges')
-      process.exit(1)
-      break
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use')
-      process.exit(1)
-      break
-    default:
-      throw error
-  }
-})
+//   switch (error.code) {
+//     case 'EACCES':
+//       console.error(bind + ' requires elevated privileges')
+//       process.exit(1)
+//       break
+//     case 'EADDRINUSE':
+//       console.error(bind + ' is already in use')
+//       process.exit(1)
+//       break
+//     default:
+//       throw error
+//   }
+// })
