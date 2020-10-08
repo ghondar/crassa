7498aaafd11841e90ef59a28418042bd296b7547
Anthony Bryan Gavilan Vinces <abgv9221@gmail.com>
2020-10-08 12:13:51 -0500

    [ FIX ] assets from server side

--- server/index.js
+++ server/index.js
@@ -2,13 +2,29 @@
 const moduleAlias = require('module-alias')
 
 // Ignore those pesky styles
-require('ignore-styles')
+// require('ignore-styles')
 
 // Set up babel to do its thing... env for the latest toys, react-app for CRA
 require('@babel/register')({
   ignore : [ /\/(build|node_modules)\/(?!crassa\b)/ ],
   presets: [ '@babel/env', '@babel/preset-react' ],
-  plugins: [ [ '@babel/plugin-proposal-class-properties', { loose: true } ], '@babel/plugin-transform-flow-strip-types', 'babel-plugin-dynamic-import-node', '@loadable/babel-plugin' ]
+  plugins: [
+    [ '@babel/plugin-proposal-class-properties', { loose: true } ],
+    '@babel/plugin-transform-flow-strip-types',
+    'babel-plugin-dynamic-import-node', '@loadable/babel-plugin',
+    [
+      'transform-assets',
+      {
+        extensions: [
+          'css',
+          'svg',
+          'png',
+          'jpg'
+        ],
+        name: 'static/media/[name].[hash:8].[ext]'
+      }
+    ]
+  ]
 })
 
 require('regenerator-runtime/runtime')
